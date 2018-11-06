f4d8005d8c5a2393130a0b836f09151512d4f896
vetoshk1n <olvetoshkin@gmail.com>
2018-11-06 18:34:07 +0200

    task finished

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const part1 = String(num).split('');
+  const part2 = part1.splice(0, part1.length / 2);
+  if (part1.join('') === part2.join('')){
+    return num;
+  }
+  return num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
+function countNumbers (number){
+  return String(number)
+    .split('')
+    .reduce((sum, item) => sum + +item, 0);
+}
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr.sort((a,b) => countNumbers(a) - countNumbers(b));
 };
